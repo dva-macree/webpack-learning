85ba275f9005eea16df893c5dcfb53ce997bc74f
zhiyuan.jiang <zhiyuan.jiang@vipshop.com>
2016-10-09 18:29:05 +0800

    修改3

--- webpack_vue/webpack.config.js
+++ webpack_vue/webpack.config.js
@@ -68,21 +68,21 @@ module.exports = {
     ]
 }
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports.plugins = [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.optimize.OccurenceOrderPlugin()
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports.plugins = [
+//     new webpack.DefinePlugin({
+//       'process.env': {
+//         NODE_ENV: '"production"'
+//       }
+//     }),
+//     new webpack.optimize.UglifyJsPlugin({
+//       compress: {
+//         warnings: false
+//       }
+//     }),
+//     new webpack.optimize.OccurenceOrderPlugin()
    
-  ]
-} else {
-  module.exports.devtool = '#source-map'
-}
+//   ]
+// } else {
+    module.exports.devtool = '#source-map'
+// }
